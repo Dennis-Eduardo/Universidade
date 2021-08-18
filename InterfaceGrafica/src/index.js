205b4c1a0f190f474b6f7f7c4215a9ab9e97ae27
Dennis-Eduardo <67655414+Dennis-Eduardo@users.noreply.github.com>
2021-08-18 20:07:32 -0300

    Update index.js

--- InterfaceGrafica/src/index.js
+++ InterfaceGrafica/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './components/Header'
+import Header from './components/Header';
 import Principal from './components/Principal';
 
 
